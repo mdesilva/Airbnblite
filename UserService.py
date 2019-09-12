444440dab3296bea3889de77a48498f6ac7eb5cf
Manuja DeSilva <mdesilva@bu.edu>
2019-09-11 20:14:48 -0400

    got authentication working

---
+++ UserService.py
@@ -0,0 +1,25 @@
+from database import DatabaseConnection
+import hashlib #to encrypt and decrypt user passwords
+
+class UserService():
+
+    def __init__(self):
+        self.db = DatabaseConnection()
+        self.collection = "users"
+    
+    def isUser(self, username):
+        if self.db.findOne(self.collection, {"username": username}):
+            return True
+        else:
+            return False
+    
+    def authenticate(self, request):
+        username = request['username']
+        password = request['password']
+        user = self.db.findOne(self.collection, {'username': username})
+        encryptedPassword = hashlib.sha256()
+        encryptedPassword.update(password.encode('UTF-8'))
+        if (encryptedPassword.hexdigest() == user['password']):
+            return True
+        else:
+            return False
--- app.py
+++ app.py
@@ -1,22 +1,51 @@
+import flask_login
 from flask import Flask, jsonify, request, Response
+from flask_login import LoginManager
 from flask_pymongo import PyMongo
 from database import DatabaseConnection
 from bson.objectid import ObjectId
-from SessionService import SessionService
 from UserController import UserController
 from PropertyController import PropertyController
+from UserService import UserService
+from User import User
 
 app = Flask(__name__)
 
+app.secret_key = "air bnb lite"
 app.config['MONGO_DBNAME'] = 'airbnblite'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/airbnblite'
 
 mongo = PyMongo(app)
 db = DatabaseConnection()
 
+UserService = UserService()
+User = User()
 UserController = UserController()
 PropertyController = PropertyController()
-SessionService = SessionService()
+SessionService = LoginManager()
+SessionService.init_app(app)
+
+#Authentication setup
+
+@SessionService.user_loader
+def user_loader(username):
+    if not UserService.isUser(username):
+        return
+    user = User
+    user.id = username
+    return user
+
+@SessionService.request_loader
+def request_loader(request):
+    if not request.form:
+        return
+    if not UserService.authenticate(request.form):
+        return
+    user = User
+    user.id = request.form["username"]
+    user.is_authenticated = True
+    return user
+
 
 @app.route('/user/signup', methods=['POST'])
 def signupUser():
@@ -24,7 +53,20 @@ def signupUser():
 
 @app.route('/user/login', methods=['POST'])
 def loginUser():
-    return UserController.login(request.form)
+    if UserService.authenticate(request.form):
+        user = User
+        user.id = request.form["username"]
+        flask_login.login_user(user)
+        return Response(status=200)
+    return Response(status=400)
+
+@app.route('/user/logout')
+def logoutUser():
+    if flask_login.current_user.id:
+        flask_login.logout_user()
+        return Response(status=200)
+    else:
+        return Response("No user currently logged in", content_type="text/html")
 
 # Authenticated Routes
 @app.route('/properties/<string:id>', methods=['GET'])
@@ -33,6 +75,7 @@ def getById(id):
     return PropertyController.get(ObjectId(id))
 
 @app.route('/properties', methods=['GET'])
+@flask_login.login_required
 def getAll():
     return PropertyController.getAllAvailableProperties()
 
@@ -44,11 +87,10 @@ def addNewProperty():
 def rentProperty():
     return PropertyController.rentProperty(request.form)
 
-@app.route('/theRequestIs', methods=['GET', 'POST'])
-@SessionService.isUserLoggedIn()
-def theRequestIs():
-    return Response("Take a look at the server", content_type="text/html")
-
+@app.route('/protected', methods=['GET'])
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4000, debug=True)
